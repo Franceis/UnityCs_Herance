f38c7825708ea899a6d461218115feef28467177
Franceis <vinicusfranca10@gmail.com>
2021-02-23 11:34:55 -0300

    commit 0

---
+++ Herance/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float life;
+    private float maxLife;
+
+    private float spBar;
+    private float spBarMax;
+
+    private float damage;
+    private float defense;
+
+    bool canAttack = true;
+
+    public virtual void Start()
+    {
+
+    }
+
+    public virtual void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Block();
+        }
+    }
+
+    private void Attack()
+    {
+        //Attack animation
+    }
+
+    private void Block()
+    {
+        //Block animation
+    }
+}
